cde126748a6892ee5b036f360c54927333ce9b28
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-10-21 02:12:17 +0100

    Nearly finalized movement. Add Tags on Bugs Scene to implement movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,9 +10,11 @@ public class Player : MonoBehaviour
     private float               jumpSpeed;
 
     private Rigidbody2D         rb;
-    private CircleCollider2D    groundChecker;
+    private GroundChecker       groundChecker;
 
-    private bool                grounded;    
+    private bool                grounded;
+    private bool                jumpStart;
+
     private float               velocity_x;
     private float               added_velocity_y;
 
@@ -20,14 +22,13 @@ public class Player : MonoBehaviour
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-
-        //Start Grounded as test until jumping is implemented and solved
-        grounded = true;      
+        groundChecker = gameObject.transform.Find("GroundChecker").GetComponent<GroundChecker>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        CheckGround();
         ReadInput();
         Move();
     }
@@ -43,17 +44,35 @@ public class Player : MonoBehaviour
             if(!(Input.GetKey("a") || Input.GetKey("d")))
                 velocity_x = 0;
 
-            if(Input.GetKey("w"))
+            if(Input.GetKeyDown("w"))
+            {
+                jumpStart = true;
                 added_velocity_y = jumpSpeed;
+            }
+            else
+                jumpStart = false;
         }
         else
         {
             added_velocity_y = 0;
+            
+            if(Input.GetKey("w"))
+                rb.gravityScale = 0.35f;
+            else
+                rb.gravityScale = 1f;
         }
     }
 
     private void Move()
     {
+        if(added_velocity_y > 0 && !jumpStart)
+            added_velocity_y = 0;
+
         rb.velocity = new Vector2(velocity_x, rb.velocity.y + added_velocity_y);
+    }
+
+    private void CheckGround()
+    {
+        grounded = groundChecker.grounded;
     }
 }
