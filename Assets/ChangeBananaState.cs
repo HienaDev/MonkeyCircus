aae0bb0fc40e4ca447e14748b5036ea71206bbb1
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-10-21 20:56:14 +0100

    Implemented banana on and off UI elements

--- Assets/ChangeBananaState.cs
+++ Assets/ChangeBananaState.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class ChangeBananaState : MonoBehaviour
 {
-    [SerializeField] private ThrowBanana monkey;
+    [SerializeField] private ThrowBanana        monkey;
 
-    [SerializeField] private 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private SpriteRenderer     banana_on;
+    [SerializeField] private SpriteRenderer     banana_off;
 
     // Update is called once per frame
     void Update()
     {
         if (monkey.GetReadyBanana())
         {
-
+            banana_on.enabled = true;
+            banana_off.enabled = false;
         }
-
+        else
+        {
+            banana_on.enabled = false;
+            banana_off.enabled = true;
+        }
     }
 }
