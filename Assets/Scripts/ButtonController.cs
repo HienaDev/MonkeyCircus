5eb2a9c596b14c113c9d2448a45344adfbc2464e
Ricardo Louro <ricardo.f.louro@gmail.com>
2023-10-22 01:53:50 +0100

    Fixed issues with Main Menu and switching control modes through it

--- Assets/Scripts/ButtonController.cs
+++ Assets/Scripts/ButtonController.cs
@@ -2,15 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class ButtonController : MonoBehaviour
 {
-    [SerializeField]
-    public Scene game;
+    private TextMeshProUGUI tmp;
+    private bool modern = false;
 
+    private void Start()
+    {
+        tmp = GameObject.Find("Controls").GetComponentInChildren<TextMeshProUGUI>();
+    }
+    
     public void StartGame()
     {
+        Player.SetControls(modern);
         SceneManager.LoadScene("Tutorial");  
+    }
+
+    public void ControlTypeToggle()
+    {
+        if(modern)
+        {
+            modern = false;
+            tmp.text = "CLASSIC CONTROLS";
+        }
+        else
+        {
+            modern = true;
+            tmp.text = "MODERN CONTROLS";
+        } 
     }
 
     public void Quit()
